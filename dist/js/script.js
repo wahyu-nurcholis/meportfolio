e68e50530d9be92fba6fd7b859685a7c6d98fb8e
Wahyu Nurcholis <whyunurc96@gmail.com>
2023-12-21 03:01:23 +0700

    upade fitu scrol dan hamburger menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,9 +10,22 @@ hamburger.addEventListener("click", function () {
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const totop = document.querySelector("#to-top");
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    totop.classList.remove("hidden");
+    totop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    totop.classList.remove("flex");
+    totop.classList.add("hidden");
   }
 };
+
+// click random to hidden hamburger menus
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
